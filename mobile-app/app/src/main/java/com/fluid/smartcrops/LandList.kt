9747b598c1b037749aa68ee6604d162ac770b55a
Muhammad Ega <muhammadega@Muhammads-MacBook-Pro.local>
2021-11-08 12:52:44 +0700

    add cost estimation

---
+++ mobile-app/app/src/main/java/com/fluid/smartcrops/LandList.kt
@@ -0,0 +1,11 @@
+package com.fluid.smartcrops
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_land_list)
+    }
+}
